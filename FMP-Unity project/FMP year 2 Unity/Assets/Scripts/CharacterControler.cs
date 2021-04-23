74e05fa6e5d47ec3ce7bcb21f318bc3d19e78a96
MinchiMuncher6969 <61968303+MinchiMuncher6969@users.noreply.github.com>
2021-04-23 16:21:49 +0100

    Added Unity

---
+++ FMP-Unity project/FMP year 2 Unity/Assets/Scripts/CharacterControler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CharacterControler : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6f;
+    public float fallGravity = 1.0f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.Move(Physics.gravity * fallGravity * Time.deltaTime);
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 MoveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(MoveDir.normalized * speed * Time.deltaTime);
+
+        }
+
+    }
+}
