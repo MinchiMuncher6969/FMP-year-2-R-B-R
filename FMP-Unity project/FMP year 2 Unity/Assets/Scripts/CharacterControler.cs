ff588bda9298f820419f0ce42bca03789c65078d
MinchiMuncher6969 <61968303+MinchiMuncher6969@users.noreply.github.com>
2021-05-06 12:07:10 +0100

    Added jumping and running
    I added jumping running, changed player model so i can animate it, and added some effects to the gun and a health system.

--- FMP-Unity project/FMP year 2 Unity/Assets/Scripts/CharacterControler.cs
+++ FMP-Unity project/FMP year 2 Unity/Assets/Scripts/CharacterControler.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 
 public class CharacterControler : MonoBehaviour
 {
-
+    private Vector3 moveDirection = Vector3.zero;
     public CharacterController controller;
     public Transform cam;
 
-    public float speed = 6f;
-    public float fallGravity = 1.0f;
+    public float speed = 100.0f;
+    public float gravity = 1.0f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+    public float jumpSpeed = 8.0F;
+    public float Running = 150.0f;
+    public float NormalSpeed = 100.0f;
+
 
     // Update is called once per frame
     void Update()
     {
-        controller.Move(Physics.gravity * fallGravity * Time.deltaTime);
+        
+        //controller.Move(Physics.gravity * gravity * Time.deltaTime);
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
@@ -32,6 +37,28 @@ public class CharacterControler : MonoBehaviour
             Vector3 MoveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(MoveDir.normalized * speed * Time.deltaTime);
 
+        }
+        {
+            if (controller.isGrounded)
+            {
+                moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+                moveDirection = transform.TransformDirection(moveDirection);
+                moveDirection *= speed;
+                if (Input.GetButton("Jump"))
+                    moveDirection.y = jumpSpeed;
+
+            }
+            moveDirection.y -= gravity * Time.deltaTime;
+            controller.Move(moveDirection * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftControl))
+        {
+            speed = Running;
+        }
+        else
+        {
+            speed = NormalSpeed;
         }
 
     }
--- FMP-Unity project/FMP year 2 Unity/Assets/Scripts/GunScript.cs
+++ FMP-Unity project/FMP year 2 Unity/Assets/Scripts/GunScript.cs
@@ -5,25 +5,37 @@ public class GunScript : MonoBehaviour {
     public float damage = 10f;
     public float range = 100f;
 
+    public ParticleSystem muzzleFlash;
+
     public GameObject Gun;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+
+        if (Input.GetKey(KeyCode.Mouse0))
         {
             Shoot();
-            Debug.Log("Shooting");
+           
+            Debug.DrawRay(transform.position, transform.forward * range, Color.yellow, 1, true);
         }
     }
 
     void Shoot ()
     {
+        muzzleFlash.Play();
+
         RaycastHit hit;
         if (Physics.Raycast(Gun.transform.position, Gun.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
             Debug.Log("Hit");
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
         }
     }
 }
